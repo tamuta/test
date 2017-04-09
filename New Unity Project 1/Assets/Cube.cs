da250fe8cb0d0aec786eda14dd33265874872862
t_tamura <tamtam2440@gmail.com>
2017-04-09 17:41:06 +0900

    printを追加

--- New Unity Project 1/Assets/Cube.cs
+++ New Unity Project 1/Assets/Cube.cs
@@ -6,8 +6,8 @@ public class Cube : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        print("aaa");
+    }
 	
 	// Update is called once per frame
 	void Update () {
